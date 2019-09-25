4732fff6ee74a678a13feb3d427f8af079d2a8f4
Camillo Fillinger <camillo.fillinger@outlook.de>
2019-09-25 09:20:26 +0200

    Tagesablauf erweitert

--- Tools/MATStoSUMO.py
+++ Tools/MATStoSUMO.py
@@ -31,14 +31,10 @@ import sumolib
 
 def get_options(args=None):
     optParser = optparse.OptionParser()
-    optParser.add_option("-n", "--net-file", dest="netfile",
-                         help="define the NetEdit filename")
-    optParser.add_option("-r", "--route-files", dest="routefiles",
-                         help="define the route file seperated by comma(mandatory)")
-    optParser.add_option("-o", "--output-file", dest="outfile",
-                         help="define the output filename")
-    optParser.add_option("-m", "--modesfile", dest="modesfile",
-                         help="Give a modesfile")
+    optParser.add_option("-r", "--route-file", dest="routefile",
+                         help="define the route file (mandatory)")
+    optParser.add_option("-o", "--out-file", dest="outfile",
+                         help="Give a outfile")
     optParser.add_option("-d", "--duration", 
                          help="Give a time, how long the vehicle stands")
     optParser.add_option("-u", "--until", 
@@ -50,48 +46,24 @@ def get_options(args=None):
 
     (options, args) = optParser.parse_args(args=args)
 
-    if not options.routefiles or not options.netfile or not options.outfile or not options.typesfile:
+    if not options.routefile or not options.outfile:
         optParser.print_help()
         sys.exit()
 
-    if not options.duration and not options.until:
-        optParser.print_help()
-        sys.exit()
     return options
-
-
-def readTypes(options):
-    vtypes = {None : "passenger"}
-    for file in options.typesfile.split(','):
-        for vtype in sumolib.output.parse(file,'vType'):
-            vtypes[vtype.id] = vtype.vClass
-    #print(vtypes)         
-    return vtypes
-    
+   
 
 def main(options):
     with open(options.outfile, 'w', encoding="utf8") as outf:
-        net = sumolib.net.readNet(options.netfile)
-        vtypes = readTypes(options)
+        idveh=0
         sumolib.writeXMLHeader(outf, "$Id$", "routes")
-        for file in options.routefiles.split(','):            
-            for veh in sumolib.output.parse(file, 'vehicle'):
-                edgesList = veh.route[0].edges.split()
-                lastEdge = net.getEdge(edgesList[-1])
-                lanes = lastEdge.getLanes()
-                for lane in lanes:                  
-                    if lane.allows(vtypes[veh.type]):
-                        stopAttrs ={"lane": lane.getID()}                       
-                        if options.parking:
-                            stopAttrs["parking"] = "true"
-                        if options.duration:
-                            stopAttrs["duration"] = options.duration
-                        if options.until:
-                            stopAttrs["until"]= options.until
-                        veh.addChild("stop",attrs=stopAttrs)                           
-                        break
-                    
-                outf.write(veh.toXML()) 
+        for leg in sumolib.output.parse(options.routefile, 'leg'):
+            if leg.mode == "car":
+                outf.write('    <vehicle id="%s" depart="%s" >\n' % (idveh , leg.dep_time))
+                outf.write('         <route edges="%s"/>\n' % (leg.route[0].getText()))
+                outf.write('    </vehicle>')
+                idveh=idveh+1
+
         outf.write('</routes>\n')
     outf.close()
 
